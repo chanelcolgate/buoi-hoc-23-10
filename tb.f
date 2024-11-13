d5bb88d24264ba844413bc92445ec76fa8a73b3f
nevermore <thanhdatbkdn14@gmail.com>
2024-11-13 20:30:06 +0700

    deleted:    sync_fifo.sv

--- tb.f
+++
@@ -1,4 +0,0 @@
-+incdir+./verif
-./verif/sfifo_pkg.sv
-./verif/sfifo_interface.sv
-./verif/tb_top.sv
--- verif/sfifo_sequencer.svh
+++
@@ -1,7 +0,0 @@
-class sfifo_sequencer extends uvm_sequencer #(sfifo_seq_item);
-  `uvm_component_utils(sfifo_sequencer)
-
-  function new(string name="sfifo_sequencer", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
